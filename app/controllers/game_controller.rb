7197bbb00e26e7a4a12661c96ceb7d8a58d464b9
rubinmd <matt@Matthews-MacBook-Pro.local>
2015-10-11 09:04:41 -0500

    RPS logic
    RPS logic added

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,11 +6,41 @@ class GameController < ApplicationController
     # Your code goes below.
     # The move the user chose is in the variable @user_move.
     # ===============================================================
-
-    @computer_move = "Replace this string with the correct value."
-
-    @result = "Replace this string with the correct value."
-
+    possible_moves=["rock","paper","scissors"]
+    @computer_move = possible_moves.sample
+if @user_move=="rock"
+    if @computer_move==@user_move
+        @result="tied"
+    end
+    if @computer_move=="paper"
+        @result="lose"
+    end
+    if @computer_move=="scissors"
+        @result="win"
+    end
+end
+if @user_move=="paper"
+    if @computer_move==@user_move
+        @result="tied"
+    end
+    if @computer_move=="rock"
+        @result="win"
+    end
+    if @computer_move=="scissors"
+        @result="lose"
+    end
+end
+if @user_move=="scissors"
+    if @computer_move==@user_move
+        @result="tied"
+    end
+    if @computer_move=="rock"
+        @result="lose"
+    end
+    if @computer_move=="paper"
+        @result="win"
+    end
+end
     # ===============================================================
     # Your code goes above.
     # ===============================================================
